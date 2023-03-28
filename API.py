0b744dca34085f4182c51e76d8f76df09409e0ac
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-03-28 06:02:10 -0500

    Four new functions were added to get user data. The scopes array also was modified to get the authorization required

--- API.py
+++ API.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import json
 
 sp = None
-SCOPE = ['user-library-read']
+SCOPE = ['user-library-read','user-top-read','user-read-recently-played']
 
 def authorization(client_id, client_secret, redirect_uri):
     global sp
@@ -23,3 +23,26 @@ def get_user_info()->None:
 
     with open('current_user_saved_albums.json', 'w') as archivo:
         json.dump(sp.current_user_saved_albums(), archivo, indent=4)
+
+def get_recently_played_songs_json()->None:
+    global sp
+    with open('user-json/recently_played_songs.json', 'w') as archivo:
+        json.dump(sp.current_user_recently_played(50), archivo, indent=4)
+
+def get_user_top_artists()->None:
+    global sp
+    with open('user-json/user_top_artists.json', 'w') as archivo:
+        json.dump(sp.current_user_top_artists(50,5,"long_term"), 
+                  archivo, indent=4)
+
+def get_user_top_tracks()->None:
+    global sp
+    with open('user-json/user_top_tracks.json', 'w') as archivo:
+        json.dump(sp.current_user_top_tracks(50,5,"long_term"), 
+                  archivo, indent=4)
+
+def get_user_liked_tracks()->None:
+    global sp
+    with open('user-json/user_liked_tracks.json', 'w') as archivo:
+        json.dump(sp.current_user_saved_tracks(50,5), 
+                  archivo, indent=4)
