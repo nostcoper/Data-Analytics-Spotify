760d2193665bec5e60ebdad719edfbb09b8f7729
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-03-29 19:50:17 -0500

    Santi created the 'get_top_artists_dict' function
    It is going to get all the artists present in the 'user_top_artist'
    JSON to create a dictionary with the name of the artist as a
    key and its default frequency value as zero

--- data-recolection/pareto-data/collect_data.py
+++ data-recolection/pareto-data/collect_data.py
@@ -4,3 +4,10 @@ def get_top_artists_json()->dict:
     with open('user-json/user_top_artists.json', 'r') as archivo:
         return json.load(archivo)
 
+#opens the JSON top artists data and returns a dict with the artist name and a default value of 0
+def get_top_artists_dict()->dict:
+    top_artists_json = get_top_artists_json()
+    top_artists_dict = {}
+    for artist in top_artists_json['items']:
+        top_artists_dict[artist['name']] = 0
+    return top_artists_dict
