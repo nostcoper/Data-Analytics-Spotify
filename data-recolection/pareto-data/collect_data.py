912acf05b0c87da667eb44bc57af2018eb995169
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-03-30 17:09:08 -0500

    Saved and tops songs gotten by Santi. Two new functions were added. One that returns a dict with all the saved songs, and the one that returns a dict with the top songs.

--- data-recolection/pareto-data/collect_data.py
+++ data-recolection/pareto-data/collect_data.py
@@ -11,3 +11,13 @@ def get_top_artists_dict()->dict:
     for artist in top_artists_json['items']:
         top_artists_dict[artist['name']] = 0
     return top_artists_dict
+
+#gets the JSON top tracks data from the user-json folder
+def get_top_tracks_json()->dict:
+    with open('user-json/user_top_tracks.json', 'r') as archivo:
+        return json.load(archivo)
+    
+#gets the JSON saved tracks data from the user-json folder
+def get_saved_tracks_json()->dict:
+    with open('user-json/user_liked_tracks.json', 'r') as archivo:
+        return json.load(archivo)
