f1baa9e9092aad01fbbf5808be0a103e5628e9e0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-01 11:19:48 -0500

    Santi adapted the collect data functions Now, there are two functions. One that creates a dict with the top artists and another one that counts the artists present in a JSON stored in the execution memory.

--- data-recolection/pareto-data/collect_data.py
+++ data-recolection/pareto-data/collect_data.py
@@ -1,43 +1,16 @@
 import json
-#gets the JSON top artists data from the user-json folder
-def get_top_artists_json()->dict:
-    with open('user-json/user_top_artists.json', 'r') as archivo:
-        return json.load(archivo)
 
-#opens the JSON top artists data and returns a dict with the artist name and a default value of 0
-def get_top_artists_dict()->dict:
-    top_artists_json = get_top_artists_json()
+#gets the API top artists data as a dict and returns a dict with the artist name and a default value of 0
+def get_top_artists_dict(top_artists_json:dict)->dict:
     top_artists_dict = {}
     for artist in top_artists_json['items']:
         top_artists_dict[artist['name']] = 0
     return top_artists_dict
 
-#gets the JSON top tracks data from the user-json folder
-def get_top_tracks_json()->dict:
-    with open('user-json/user_top_tracks.json', 'r') as archivo:
-        return json.load(archivo)
-    
-#gets the JSON saved tracks data from the user-json folder
-def get_saved_tracks_json()->dict:
-    with open('user-json/user_liked_tracks.json', 'r') as archivo:
-        return json.load(archivo)
-    
-#opens the JSON top tracks data and counts the artists in the top tracks and adds them to the top artists dict
-def count_artists_in_top_tracks_dict(top_artists_dict:dict)->dict:
-    top_tracks_json = get_top_tracks_json()
-    for track in top_tracks_json['items']:
-        for artist in track['artists']:
-            try:                
-                top_artists_dict[artist['name']] += 1
-            except KeyError: 
-                continue
-    return top_artists_dict
-
-#opens the JSON saved tracks data and counts the artists in the saved tracks and adds them to the top artists dict
-def count_artists_in_saved_tracks_dict(top_artists_dict:dict)->dict:
-    saved_tracks_json = get_saved_tracks_json()
-    for track in saved_tracks_json['items']:
-        for artist in track['track']['artists']:
+#gets a JSON from the API and counts the artists in the JSON and adds them to the top artists dict
+def count_artists_in_json(top_artists_dict:dict, json:dict)->dict:
+    for item in json['items']:
+        for artist in item['track']['artists']:
             try:
                 top_artists_dict[artist['name']] += 1
             except KeyError:
