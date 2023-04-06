880a3f90206d7860349e10c4e48fe99a834a85d1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-05 19:34:22 -0500

    Modularice the Pareto chart creation

--- statistics_processes/pareto_chart/processing_pareto_data.py
+++ statistics_processes/pareto_chart/processing_pareto_data.py
@@ -15,15 +15,23 @@ def add_cumulative_percentaje_column(df:pd.DataFrame)->pd.DataFrame:
     df['Cumulative Percentaje'] = df['Count'].cumsum() / df['Count'].sum() * 100
     return df
 
-#create a pareto chart from the pandas dataframe
-def create_pareto_chart(df:pd.DataFrame)->None:
-    fig, ax = plt.subplots(figsize=(12, 8))
+def create_bar_chart(df: pd.DataFrame, ax: plt.Axes) -> None:
     ax.bar(df['Artist'], df['Count'], color='C0')
     ax.set_ylabel('Count')
     ax.set_xlabel('Artists')
+
+def create_line_chart(df: pd.DataFrame, ax: plt.Axes) -> None:
     ax2 = ax.twinx()
     ax2.plot(df['Artist'], df['Cumulative Percentaje'], color='C1', marker='D', ms=7)
     ax2.yaxis.set_major_formatter(PercentFormatter())
     ax2.set_ylabel('Cumulative Percentaje')
+
+def set_xticklabels(ax: plt.Axes) -> None:
     plt.setp(ax.get_xticklabels(), rotation=90, ha='right')
+
+def create_pareto_chart(df: pd.DataFrame) -> None:
+    fig, ax = plt.subplots(figsize=(12, 8))
+    create_bar_chart(df, ax)
+    create_line_chart(df, ax)
+    set_xticklabels(ax)
     plt.show()
