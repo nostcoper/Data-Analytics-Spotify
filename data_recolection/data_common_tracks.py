3ded85bddea7c112e0dc5412454c57841851f435
RiosVal <riosh.valeria@gmail.com>
2023-05-02 15:35:35 -0500

    Cambios en multipage y agrego grafica de popularidad y tracks en comun

---
+++ data_recolection/data_common_tracks.py
@@ -0,0 +1,36 @@
+from API import *
+
+def create_dict_top_tracks(top_50):
+    popularity = {}
+    for item in top_50:
+        info = {}
+        track = item['track']
+        info['popularity'] = track['popularity']
+        info['id'] = track['id']
+        info['image'] = track['album']['images'][1]
+        popularity[track['name']] = info
+    return popularity
+
+def user_top_tracks() -> dict:
+    top_tracks = get_top_tracks_json(50)
+    songs = {}
+    items = top_tracks['items']
+    for item in items:
+        info = {}
+        info['popularity'] = item['popularity']
+        info['id'] = item['id']
+        info['image'] = item['album']['images'][1]
+        songs[item['name']] = info
+    return songs
+
+    
+
+def top_50_colombia() -> dict:
+    tracks_colombia = sp.playlist('37i9dQZEVXbOa2lmxNORXQ')['tracks']['items']
+    popularity = create_dict_top_tracks(tracks_colombia)
+    return popularity
+
+def top_50_global() -> dict:
+    tracks_global = sp.playlist('37i9dQZEVXbMDoHDwVN2tF')['tracks']['items']
+    popularity = create_dict_top_tracks(tracks_global)
+    return popularity
