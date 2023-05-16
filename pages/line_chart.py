c7acad876c833daacc7d6c0ad254fc8608edf6d2
RiosVal <riosh.valeria@gmail.com>
2023-05-16 15:41:24 -0500

    Update

--- pages/line_chart.py
+++ pages/line_chart.py
@@ -1,19 +1,22 @@
 import streamlit as st
-from page_config import set_page_config, set_second_pages_title
-from statistics_processes.making_line_chart import *
-from API import *
+from data_recolection.data_common_tracks import *
 
-set_page_config()
-set_second_pages_title()
+def markdown_song(dict):
+    for song in dict.keys():
+        name = song
+        image = dict[song]['image']['url']
+        st.markdown(f'<div class= "artist-list">  <img class="image-artist-list" src="{image}"><h3>{name}</h3> </div>', unsafe_allow_html=True)
 
-st.header('Grafica de popularidad de tus 15 canciones más escuchadas')
-data = trigger_line_chart()
-st.write("""
-            La popularidad de una canción es un valor entre 0 y 100, siendo 100 la más popular, se calcula mediante un algoritmo y se basa, 
-            en gran medida, en el número total de reproducciones que ha tenido la canción y en que tan recientes son.
-            En general, las canciones que se están reproduciendo mucho en la actualidad tendrán una popularidad más alta que las canciones que se 
-            reproducían mucho en el pasado. Las canciones duplicadas (por ejemplo, la misma canción de un sencillo y un álbum) se califican de forma 
-            independiente.""")
-with st.container():
-    st.bar_chart(data.set_index('names').sort_values(by='popularity', ascending=False), height=600, width=25)
-    
+
+def verify_tracks_in_common(top_50):
+    common_tracks = []
+    top_user = user_top_tracks()
+    for general_top, user_top in zip(top_50.keys(), top_user.keys()):
+        if general_top == user_top:
+            common_tracks.append(top_50[general_top])
+    if common_tracks != []:
+        for track in common_tracks:
+            markdown_song(track)
+    else:
+        st.markdown(f'<div class="regular-text">¡Eres diferente!</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="regular-text">No tienes nada en común con los top 50 de colombia</div>', unsafe_allow_html=True)
