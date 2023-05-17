3cf874e377724bcfac228fa990e4dcc257bd15f8
RiosVal <riosh.valeria@gmail.com>
2023-05-17 10:15:29 -0500

    Prueba de recolección de datos generos

--- pruebas.py
+++ pruebas.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import streamlit as st
+from API import get_top_tracks_json
 
-# Texto de ejemplo
-texto = ['Amor', 'Amor', 'Amor', 'Amor', 'Musica', 'Musica', 'Musica', 'Musica', 'Perdón', 'Perdón', 'Generosidad']
-texto = ' '.join(texto)
+def trigger_wordcloud(genres: list):
+    genres = ' '.join(genres)
+    fig, ax = plt.subplots()
+    wordcloud = WordCloud().generate(genres)
+    ax.imshow(wordcloud, interpolation='bilinear')
+    ax.axis('off')
+    st.pyplot(fig)
 
-# Crear el objeto WordCloud
-nube_palabras = WordCloud().generate(texto)
-
-# Mostrar la nube de palabras utilizando matplotlib
-plt.imshow(nube_palabras, interpolation='bilinear')
-plt.axis('off')
-plt.show()
+user_top = get_top_tracks_json(50)
+items = user_top['items']
+for item in items:
+    album = item['album']
+    print(album['genres'])
+    print('*')
