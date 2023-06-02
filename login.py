5227d5f2e9108100b71926420b0d30098a1ae05f
Dios <Dios@gmail.com>
2023-06-02 08:11:43 -0500

    try recommendation

--- login.py
+++ login.py
@@ -26,9 +26,11 @@ with login:
         st.markdown('''<div class="container-image-home">
                             <img class="image-home" src="https://play-lh.googleusercontent.com/P2VMEenhpIsubG2oWbvuLGrs0GyyzLiDosGTg8bi8htRXg9Uf0eUtHiUjC28p1jgHzo">
                        </div>''', unsafe_allow_html=True)
-
         if click_button:
-            #check_autorization, display_name = authorization('41496547b72b4bb293055ca32f73069c', 'c23c8e7a2a39428889be937501708a5e', 'http://localhost:8080')
+            try:
+                os.remove(".cache")
+            except FileNotFoundError:
+                pass
             check_autorization, display_name = authorization('f05c926d1d644bc8b3662d47f1849af2', '30392342f26f4e688435ffbc80639ee1', 'http://localhost:8080')
             #check_autorization, display_name = authorization(client_id, client_secret, re_direct_url)
             
